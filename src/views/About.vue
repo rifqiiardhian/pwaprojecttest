5cd9883f9eb99becc55822298f8b961bd37298bb
rifqiiardhian <rifqiardhian@gmail.com>
2020-05-16 01:05:45 +0700

    commit uas4

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,13 +5,42 @@
                 <img src="@/assets/3.png" class="img-fluid"/>
             </div>
             <div class="col-md-6 p-4">
-                <h1 class="mb-5">ABOUT US<span class="text-danger">.</span></h1>
-                <p class="text-justify">
-                    Hello my name is Rifqi Ardhian and it's my FInal Exam Projects.
-                    This website is about Meal API Website. Inside it, there is a
-                    food catalog page, category, meals area, and many more.
+                <div class="facts">
+                    <h1 class="mb-4">ABOUT US<span class="text-danger">.</span></h1>
+                    <p class="text-justify mb-5">
+                        Hello my name is Rifqi Ardhian and it's my Final Exam Projects.
+                        This website is about Meal API Website. Inside it, there is a
+                        food catalog page, category, meals area, and many more. The content
+                        and style of the website originally created and develop by Rifqi Ardhian.
+                        Restricted and no copying for other website.
+                    </p>
+                    <h4 class="mb-4">What is mealapp ?</h4>
+                    <p class="text-justify">
+                        Mealapp is a frontend website that diplayed a foods content, include foods
+                        category,foods country area, ingredients, and cooking steps with video.
+                        You can save your favourites foods on fav feature, you can also search foods
+                        by catgeory, areas, and ingredient. Thanks for watching our my website. Hope
+                        you glad. Do what you love and so many reasons to be happy.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row row-about p-5 mt-5">
+            <div class="col-md-6 p-4 facts">
+                <h1 class="mb-4">Did you know ?</h1>
+                <h4 class="mb-5">That potatoes can absorb and reflect Wi-fi signals</h4>
+                <p class="text-justify mb-5">
+                    When Boeing wanted to test out its wireless signal on new planes in 2012,
+                    they placed giant piles of potatoes on seats. Because of their high water
+                    content and chemical makeup, potatoes absorb and reflect radio and wireless
+                    signals just like humans do.
                 </p>
+            </div>
+            <div class="col-md-6 p-4">
+                <img src="@/assets/potato.png" class="img-fluid"/>
             </div>
         </div>
     </div>
 </template>
+<style scoped>
+</style>
